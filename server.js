940e3ceadd557bc556ca64b9f1068efee0fa2e1e
Andres Rodriguez <rod16015@byui.edu>
2019-07-13 20:43:25 -0300

    Changed the main project

--- server.js
+++ server.js
@@ -1,58 +1,52 @@
+const argv = require('yargs').argv;
 var express = require ('express');
 var app = express();
-const { Pool } = require("pg")
 require('dotenv').config();
+const request = require('request');
+const bodyParser = require('body-parser');
+//const { Pool } = require("pg");
+//const connectionString = process.env.DATABASE_URL;
 
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new Pool({connectionString: connectionString});
-
- 
+//const pool = new Pool({connectionString: connectionString});
 
 const PORT = process.env.PORT || 5000
+
+let apiKey = '3ba4507141ac346f14e337a176d7c346';
+let city ='portland'; 
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+
 
 app.use(express.static("public"));
 
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.get("/index", function(req, res) {
-        console.log("Received a request for /index");
-        res.render("pages/index");
-});
+app.get('/index', function (req, res) {
+        res.render('index');
+})
 
-app.get("/showmovies", function(req, res){
-        console.log("in the showmovies page");
-        showmovies();
-        res.render("pages/showmovies");
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+        let city = req.body.city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    
+        request(url, function (err, response, body) {
+        if(err){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+            let weather = JSON.parse(body)
+            if(weather.main == undefined){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+           let weatherText = 'In ${weather.name} we have ${weather.main.temp} the pressure is ${weather.main.pressure}'
+            res.render('index', {weather: weatherText, error: null});
+        }
+        }
+        });
+})
+   
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-function showmovies(){
-        
-        var sql = "SELECT year, movie_name, category FROM movie";
-
-        pool.query(sql, function(err, db_result) {
-        // If an error occurred...
-        if (err) {
-        console.log("Error in query: ")
-        console.log(err);
-        }
-        else{
-                var results = {
-                success:true,
-                list:db_result.rows
-        };
-        }                              
-        for (var i = 0; i < results.length; i++) {
-                var movie = list[i];
-
-        $("#ulMovies").append("<li>" + movie.movie_name + " " + movie.category + ":" + movie.year + "</li>");
-        }
-        
-        // Log this to the console for debugging purposes.
-        console.log("Back from DB with result:");
-        console.log(db_result.rows);
-});     
-}
