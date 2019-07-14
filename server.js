3ab4db6e861bb5ca5ee2dff510c71250c161198d
Andres Rodriguez <rod16015@byui.edu>
2019-07-13 23:39:59 -0300

    Changed the main project

--- server.js
+++ server.js
@@ -1,52 +1,40 @@
-const argv = require('yargs').argv;
-var express = require ('express');
-var app = express();
-require('dotenv').config();
-const request = require('request');
+const express = require('express');
 const bodyParser = require('body-parser');
-//const { Pool } = require("pg");
-//const connectionString = process.env.DATABASE_URL;
+const request = require('request');
+const app = express()
 
-//const pool = new Pool({connectionString: connectionString});
+const apiKey = '466d776634a54f08c51310cb0642c712';
 
 const PORT = process.env.PORT || 5000
 
-let apiKey = '3ba4507141ac346f14e337a176d7c346';
-let city ='portland'; 
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
-
-
-app.use(express.static("public"));
-
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-app.get('/index', function (req, res) {
-        res.render('index');
-})
-
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
 
 app.post('/', function (req, res) {
-        let city = req.body.city;
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-    
-        request(url, function (err, response, body) {
-        if(err){
-            res.render('index', {weather: null, error: 'Error, please try again'});
-        } else {
-            let weather = JSON.parse(body)
-            if(weather.main == undefined){
-            res.render('index', {weather: null, error: 'Error, please try again'});
-        } else {
-           let weatherText = 'In ${weather.name} we have ${weather.main.temp} the pressure is ${weather.main.pressure}'
-            res.render('index', {weather: weatherText, error: null});
-        }
-        }
-        });
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `In ${weather.name} we have ${weather.main.temp} degrees, the presure is ${weather.main.pressure}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
-   
+
+
 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
